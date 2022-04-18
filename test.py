1311e6b5a268aa0a839d059f15693c8f0221a593
linkidea <102345777+linkidea@users.noreply.github.com>
2022-04-19 00:30:51 +0900

    Update test.py

--- test.py
+++ test.py
@@ -21,35 +21,38 @@ driver.implicitly_wait(2)
 
 
 # 원하는 날짜 넣기
-sedate=("2022-04-26","2022-04-30")
+sedate=("2022-04-23","2022-04-30","2022-05-07","2022-05-14","2022-05-21","2022-04-27")
 
-while True :
-    for i in sedate :
-        url = 'https://imjingakcamping.co.kr/resv/res_01.html?checkdate='+i
+while True:
+    for i in sedate:
+        url = 'https://imjingakcamping.co.kr/resv/res_01.html?checkdate=' + i
         driver.get(url)
+        print(url)
+
         html = driver.find_element_by_css_selector('#tableRemainTbody').get_attribute('innerHTML')
         soup = BeautifulSoup(html, 'html.parser')
-        data = [] # 원하는 자리 담기
-        
-        for ele in soup.find_all('span',limit=2):
+        data = []  # 원하는 자리 담기
+
+        for ele in soup.find_all('span', limit=2):
             a = ele.get_text()
             num = int(a)
-            
-            if num >= 1 :
-                data.append(num)
-                
-        if data:
-            print("자리 났음!! 종료")
-            bot = telegram.Bot(token='5379588207:AAFanpXE3ts0mVstRUCOdUL8tHIdsrEVv5w')
-            chat_id = 5354867788
-            bot.sendMessage(chat_id=chat_id, text="자리 났음!! 종료") 
-            driver.quit() # 드라이버 완전히 종료. 창 하나만 닫으려면 .close()
-            break
-    
-    # 180초마다 새로고침하여 반복
-    time.sleep(180)
-    driver.refresh()
+            print(num)
 
+            if num >= 1:
+                data.append(num)
+                print(data)
+
+    if data:
+        print(i + " 자리 났음! 종료")
+        bot = telegram.Bot(token='5379588207:AAFanpXE3ts0mVstRUCOdUL8tHIdsrEVv5w')
+        chat_id = 5354867788
+        bot.sendMessage(chat_id=chat_id, text=i + " 자리 났음! 빨리 예약하세요!")
+        driver.quit()  # 드라이버 완전히 종료. 창 하나만 닫으려면 .close()
+        break
+
+        # 180초마다 새로고침하여 반복
+    time.sleep(10)
+    driver.refresh()
 
 
 
