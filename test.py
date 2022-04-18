9b3334ca3a652938d32a6d15310102f03349a8bc
linkidea <102345777+linkidea@users.noreply.github.com>
2022-04-19 01:00:15 +0900

    Update test.py

--- test.py
+++ test.py
@@ -21,7 +21,7 @@ driver.implicitly_wait(2)
 
 # 원하는 날짜 넣기
 # sedate=("2022-04-23","2022-04-30","2022-05-07","2022-05-14","2022-05-21","2022-04-27")
-sedate=("2022-05-21","2022-04-27")
+sedate=("2022-04-27")
 
 while True:
     for i in sedate:
@@ -168,3 +168,4 @@ while True:
 #     # 새로고침 10초
 #     time.sleep(10)
 #     driver.refresh()
+ 
