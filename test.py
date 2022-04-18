971dc1e5b527940770a474b855c8d4a72ceb0e47
linkidea <102345777+linkidea@users.noreply.github.com>
2022-04-18 23:54:21 +0900

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,6 @@ import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
-from selenium.webdriver.common.by import By
 
 chrome_driver = os.path.join('chromedriver')
 chrome_options = webdriver.ChromeOptions()
@@ -15,10 +14,11 @@ chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-blink-features=AutomationControlled')
 chrome_options.add_argument('User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36')
 driver = webdriver.Chrome(chrome_driver, options=chrome_options)
-driver.implicitly_wait(2)
+driver.implicitly_wait(3)
 
 # 원하는 날짜 넣기
-sedate=("2022-04-23","2022-04-30","2022-05-07","2022-05-14","2022-05-21","2022-04-27")
+# sedate=("2022-04-23","2022-04-30","2022-05-07","2022-05-14","2022-05-21","2022-04-27")
+sedate=("2022-04-23","2022-04-27")
 
 while True :
     for i in sedate :
@@ -26,7 +26,7 @@ while True :
         driver.get(url)
         print(url)
 
-        html = driver.find_element(By.CSS_SELECTOR,'#tableRemainTbody').get_attribute('innerHTML')
+        html = driver.find_element_by_css_selector('#tableRemainTbody').get_attribute('innerHTML')
         soup = BeautifulSoup(html, 'html.parser')
         data = [] # 원하는 자리 담기
 
@@ -38,25 +38,20 @@ while True :
             if num >= 1 :
                 data.append(num)
 
-        if data:
-            print("자리 났음!! 종료")
-
-            bot = telegram.Bot(token='5379588207:AAFanpXE3ts0mVstRUCOdUL8tHIdsrEVv5w')
-            chat_id = 5354867788
-            bot.sendMessage(chat_id=chat_id, text=i+ " 자리 났음! 빨리 예약하세요!")
-
-            driver.quit() # 드라이버 완전히 종료. 창 하나만 닫으려면 .close()
-            break
+                bot = telegram.Bot(token='5379588207:AAFanpXE3ts0mVstRUCOdUL8tHIdsrEVv5w')
+                chat_id = 5354867788
+                bot.sendMessage(chat_id=chat_id, text=i+ " 자리 났음! 빨리 예약하세요!")            
+                print("자리 났음!! 종료")
+                driver.quit() # 드라이버 완전히 종료. 창 하나만 닫으려면 .close()
+                break
 
         # 각 날짜별 인터벌 3초
         time.sleep(3)
         driver.refresh()
 
-    # 새로고침 10초
-    time.sleep(10)
-    driver.refresh()
-
-
+# 새로고침 10초
+time.sleep(20)
+driver.refresh()
 
 
 # from selenium import webdriver
